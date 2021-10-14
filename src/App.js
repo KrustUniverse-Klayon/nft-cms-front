32b21080a05e9bafb96414a4d5b3724c3dbdff3c
Martin Kim <martin.m@krustuniverse.com>
2021-10-14 19:59:56 +0900

    login, register 창 세팅

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ const loading = (
 const TheLayout = React.lazy(() => import('./containers/TheLayout'));
 
 // Pages
-const Login = React.lazy(() => import('./examples/pages/login/Login'));
-const Register = React.lazy(() => import('./examples/pages/register/Register'));
+const Login = React.lazy(() => import('./views/pages/Login'));
+const Register = React.lazy(() => import('./views/pages/Register'));
 const Page404 = React.lazy(() => import('./examples/pages/page404/Page404'));
 const Page500 = React.lazy(() => import('./examples/pages/page500/Page500'));
 
@@ -26,11 +26,12 @@ class App extends Component {
       <BrowserRouter>
           <React.Suspense fallback={loading}>
             <Switch>
-              <Route path="/" name="Home" render={props => <TheLayout {...props}/>} />
               <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
               <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
               <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
               <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
+              <Route path="/" name="Home" render={props => <TheLayout {...props}/>} />
+
             </Switch>
           </React.Suspense>
       </BrowserRouter>
